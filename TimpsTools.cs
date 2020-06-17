9997ae014a70d0cbb1a64b7397ef66e489e66536
Timps <thanslow@gmail.com>
2020-06-18 00:48:15 +1000

    Add files via upload

---
+++ TimpsTools.cs
@@ -0,0 +1,16 @@
+﻿//C# Example (LookAtPoint.cs)
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TimpsTools : MonoBehaviour
+{
+    [MenuItem("Tools/Timps/Remove Parents")]
+    public static void Execute()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            obj.transform.parent = null;
+        }
+    }
+}
